534b6312845331d82dfbd28419101579e5be143b
Sreshta <121309862+sreshtech@users.noreply.github.com>
2023-12-09 03:30:19 +0530

    Update zerosonestwos.java

--- array/zerosonestwos.java
+++ array/zerosonestwos.java
@@ -1,3 +1,4 @@
+//COMPLEXITY WILL BE O(N)
 import java.util.* ;
 import java.io.*; 
 public class Solution {
@@ -24,4 +25,32 @@ public class Solution {
             arr.set(i,2);
         }
     }
+}
+
+//COMPLEXITY O(1) 
+class Solution {
+    public void sortColors(int[] nums) {
+        int low = 0, mid = 0, high = nums.length - 1; 
+        while (mid <= high) {
+            if (nums[mid] == 0) { 
+                swap(nums,low, mid);
+                low++;
+                mid++;
+            }
+            else if (nums[mid] == 1) {
+                mid++;
+            }
+            else {
+                swap(nums,mid,high);
+                high--;
+            }
+        }
+    }
+    static void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+  
+    
 }
