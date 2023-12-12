ca464a3d3f02eecb4ad744375d89e10fec52762d
Sreshta <121309862+sreshtech@users.noreply.github.com>
2023-12-13 04:14:54 +0530

    Update MajorityElement.java

--- array/MajorityElement.java
+++ array/MajorityElement.java
@@ -1,3 +1,4 @@
+//time complexity high
 class Solution {
     public int majorityElement(int[] nums) {
         int n = nums.length;
@@ -19,5 +20,30 @@ class Solution {
         }
 
         return -1;
+    }
+}
+//better time complexity
+public class Solution {
+    public static int majorityElement(int[] nums) {
+        int count = 0;
+        Integer candidate = null;
+
+        for (int num : nums) {
+            if (count == 0) {
+                candidate = num;
+            }
+            count += (num == candidate) ? 1 : -1;
+        }
+
+        // At this point, the candidate should be the majority element,
+        // but it's essential to verify if it occurs more than n/2 times.
+        count = 0;
+        for (int num : nums) {
+            if (num == candidate) {
+                count++;
+            }
+        }
+
+        return (count > nums.length / 2) ? candidate : -1;
     }
 }
